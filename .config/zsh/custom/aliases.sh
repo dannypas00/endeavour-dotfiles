068c1bcdc3e5a2e8711f4d24deecb24597e21e0f
Danny Pas <dannypas@canvascompany.nl>
2022-11-29 08:54:20 +0100

    Added aliases and keybinds for whole word stuff

---
+++ .config/zsh/custom/aliases.sh
@@ -0,0 +1,5 @@
+alias datapkg='cp -r $HOME/web/data/src $HOME/web/grutte-apier/vendor/fotocadeau/data/ && cp -r $HOME/web/data/src $HOME/web/laravel-backend/vendor/fotocadeau/data/ && composer --working-dir=$HOME/web/grutte-apier dump --optimize --no-scripts && composer --working-dir=$HOME/web/laravel-backend dump --optimize --no-scripts'
+alias dbj='docker restart redis horizon image-generator-horizon billy-beauty-shot-horizon && docker exec -it image-generator php artisan ean:fill-redis && docker exec -it grutte-apier php artisan database-jobs:start && docker exec -it image-generator php artisan database-jobs:start';
+alias dbj-billy='docker restart redis billy-beauty-shot-horizon && docker exec -it image-generator php artisan ean:fill-redis && docker exec -it grutte-apier php artisan database-jobs:start && docker exec -it image-generator php artisan database-jobs:start';
+alias dbj-grutte='docker restart redis horizon && docker exec -it grutte-apier php artisan database-jobs:start';
+alias dbj-gert='docker restart redis image-generator-horizon && docker exec -it image-generator php artisan ean:fill-redis && docker exec -it image-generator php artisan database-jobs:start';
